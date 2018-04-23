cd5db32ce18198261908b353461ae164acf2f185
Chanple <chanple@outlook.com>
2018-04-23 22:00:20 +0800

    ~
    ~

---
+++ SQLSwitcher/SQLSwitcher/IconTray.cs
@@ -0,0 +1,41 @@
+﻿using SQLSwitcher.Properties;
+using System;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.IO;
+
+namespace SQLSwitcher
+{
+    public class IconTray
+    {
+        private NotifyIcon notifyIcon;
+        private ServiceController sc;
+
+
+        public IconTray()
+        {
+            sc = new ServiceController("MSSQLSERVER");
+            notifyIcon = new NotifyIcon
+            {
+                Icon = sc.Status.Equals(ServiceControllerStatus.Stopped) ? Resources.server_stop : Resources.server_run,
+                Visible = true
+            };
+
+            notifyIcon.MouseDoubleClick += (s, e) =>
+            {
+                Process.Start(Path.Combine(Environment.CurrentDirectory, "Switcher"));
+                if (sc.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+                    sc.WaitForStatus(ServiceControllerStatus.Running);
+                    notifyIcon.Icon = Resources.server_run;
+                }
+                else
+                {
+                    sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                    notifyIcon.Icon = Resources.server_stop;
+                }
+            };
+        }
+    }
+}
---
+++ SQLSwitcher/Switcher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+
+namespace Switcher
+{
+    static class Program
+    {
+        static void Main()
+        {
+            using (ServiceController sc = new ServiceController("MSSQLSERVER"))
+            {
+                if (sc.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+                    sc.Start();
+                }
+                else
+                {
+                    sc.Stop();
+                }
+            }
+        }
+    }
+}
