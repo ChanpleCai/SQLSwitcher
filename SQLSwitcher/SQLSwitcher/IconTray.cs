4c77a783c682c0cac7a116f1bf06f16b5d2a29ca
Chanple <chanple@outlook.com>
2019-03-02 22:49:13 +0800

    add asynchronous

--- SQLSwitcher/SQLSwitcher/IconTray.cs
+++ SQLSwitcher/SQLSwitcher/IconTray.cs
@@ -2,66 +2,67 @@
 using System.Windows.Forms;
 using System.ServiceProcess;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace SQLSwitcher
 {
     public class IconTray : ApplicationContext
     {
-        private NotifyIcon notifyIcon;
+        private NotifyIcon _notifyIcon;
         private ServiceController sc;
-        private readonly bool IsChinese = System.Threading.Thread.CurrentThread.CurrentUICulture.Name.StartsWith("zh");
+        private readonly bool _isChinese = System.Threading.Thread.CurrentThread.CurrentUICulture.Name.StartsWith("zh");
 
 
         public IconTray()
         {
             sc = new ServiceController("OracleServiceORCL");
-            notifyIcon = new NotifyIcon
+            _notifyIcon = new NotifyIcon
             {
                 Icon = sc.Status.Equals(ServiceControllerStatus.Stopped) ? Resources.server_stop : Resources.server_run,
                 Text = Application.ProductName,
                 Visible = true
             };
 
-            notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+            _notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
 
             var exit = new ToolStripMenuItem
             {
                 Font = new System.Drawing.Font("Segoe UI", 9F),
-                Text = IsChinese ? "退出" : "Exit"
+                Text = _isChinese ? "退出" : "Exit"
             };
 
             exit.Click += (s, e) =>
             {
-                notifyIcon.Dispose();
+                _notifyIcon.Dispose();
                 sc.Dispose();
                 Application.Exit();
             };
 
             var contextMenuStrip = new ContextMenuStrip();
             contextMenuStrip.Items.Add(exit);
-            notifyIcon.ContextMenuStrip = contextMenuStrip;
+            _notifyIcon.ContextMenuStrip = contextMenuStrip;
         }
 
-        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        private async void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            notifyIcon.MouseDoubleClick -= NotifyIcon_MouseDoubleClick;
+            _notifyIcon.MouseDoubleClick -= NotifyIcon_MouseDoubleClick;
             Process.Start("schtasks", "/run /TN \"SQLSwitcher\"");
 
             if (sc.Status.Equals(ServiceControllerStatus.Stopped))
             {
-                sc.WaitForStatus(ServiceControllerStatus.Running);
-                notifyIcon.Icon = Resources.server_run;
-                notifyIcon.Text = notifyIcon.BalloonTipText = IsChinese ? "SQL 服务正在运行" : "SQL Server is running!";
+                await Task.Run(() => sc.WaitForStatus(ServiceControllerStatus.Running));
+                _notifyIcon.Icon = Resources.server_run;
+                _notifyIcon.Text = _notifyIcon.BalloonTipText = _isChinese ? "SQL 服务正在运行" : "SQL Server is running!";
             }
             else
             {
-                sc.WaitForStatus(ServiceControllerStatus.Stopped);
-                notifyIcon.Icon = Resources.server_stop;
-                notifyIcon.Text = notifyIcon.BalloonTipText = IsChinese ? "SQL 服务已停止" : "SQL Server has stopped!";
+                await Task.Run(() => sc.WaitForStatus(ServiceControllerStatus.Stopped));
+                _notifyIcon.Icon = Resources.server_stop;
+                _notifyIcon.Text = _notifyIcon.BalloonTipText = _isChinese ? "SQL 服务已停止" : "SQL Server has stopped!";
             }
 
-            notifyIcon.ShowBalloonTip(3000);
-            notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+            _notifyIcon.ShowBalloonTip(3000);
+            _notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
         }
     }
 }
